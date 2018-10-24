f95c33763921a62c60ca96269cea88945055f533
Tobias Zipfel <tobias.zipfel@de.ibm.com>
2018-10-24 12:12:10 +0200

    add info endpoint
    Signed-off-by: Norman Sutorius <norman.sutorius@de.ibm.com>

--- main.go
+++ main.go
@@ -4,13 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("hello, world!")
-		fmt.Fprint(w, "hello, world!")
+		fmt.Println("CF_INSTANCE_IP", os.Getenv("CF_INSTANCE_IP"))
+		fmt.Fprintf(w, "CF_INSTANCE_IP", os.Getenv("CF_INSTANCE_IP"))
 	})
+	http.HandleFunc("/panic", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Content-Length", "100000")
+		panic("I panicked")
+	})
+
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
